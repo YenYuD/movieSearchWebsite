fba03136c2f4e36d496192e78cedc8ab6dd98c29
YenYuD <diaoemily0830@gmail.com>
2023-03-23 22:54:42 +0800

    解決build error

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,13 @@
-import { Grid } from '@mui/material'
+
 import Image from 'next/image'
 import React from 'react'
 
 const NotFoundPage = () => {
     return (
-        <Grid className="h-screen pt-40 flex flex-col items-center gap-14">
+        <div className="h-screen pt-40 flex flex-col items-center gap-14">
             <div>Page not found. sorry.....</div>
             <Image src="/images/dtmdh6nbxsx81.png" alt="cat crying image" className="object-cover " width={500} height={500} />
-        </Grid>
+        </div>
 
     )
 }
