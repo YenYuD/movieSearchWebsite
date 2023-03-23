af6f79357228e55fce469a0290d9c4122e4ce8dc
YenYuD <diaoemily0830@gmail.com>
2023-03-24 00:54:38 +0800

    debugging...

--- libs/loadComments.ts
+++ libs/loadComments.ts
@@ -1,5 +1,5 @@
 export async function getComments(movieID: number) {
-    const res = await fetch("http://localhost:3000/api/comments/" + movieID, {
+    const res = await fetch("/api/comments/" + movieID, {
         method: "GET",
     });
     const data = await res.json();
