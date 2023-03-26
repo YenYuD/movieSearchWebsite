a3d3e08049c4949f90093af16be49a2b184cf4c3
YenYuD <diaoemily0830@gmail.com>
2023-03-26 19:31:28 +0800

    更新mongoDB 連線字串

--- helpers/db-util.ts
+++ helpers/db-util.ts
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 export async function connectDatabase() {
     const client = await MongoClient.connect(
-        `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_DB_NAME}.xwkdcly.mongodb.net/?retryWrites=true&w=majority`
+        `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_DB_NAME}.fymps2b.mongodb.net/?retryWrites=true&w=majority`
     );
 
     return client;
