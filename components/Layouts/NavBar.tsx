4e8376f8dcf7ce191fe42a31aef5d634871c6138
YenYuD <diaoemily0830@gmail.com>
2023-03-28 16:29:53 +0800

    navbar 在捲軸往下捲時收起，往上捲時顯示

--- components/Layouts/NavBar.tsx
+++ components/Layouts/NavBar.tsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import Hamburger from "./Hamburger";
 
 const NavBar = () => {
+
+    const [showNav, setShowNav] = useState(false);
+
+    const nav = useRef(null);
+
+    const handleScroll = () => {
+
+        const scrollY = window.scrollY;
+
+        if (scrollY === 0) {
+            setShowNav(true);
+        }
+
+        if (scrollY < previousScrollY) {
+            setShowNav(true);
+        } else {
+            setShowNav(false);
+        }
+
+        previousScrollY = scrollY;
+    };
+
+    let previousScrollY = 0;
+
+    useEffect(() => {
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+
+    }, [])
+
     return (
         <>
             <nav >
                 <Hamburger />
-                <div className="hidden my-6 fixed w-full px-16 z-20 justify-between sm:flex ">
+                <div ref={nav} className={`${showNav ? "" : '-translate-y-full'} hidden my-6 fixed w-full px-16 z-20 justify-between sm:flex `}>
                     <div className="h-14 w-full ">
                         <ul className="flex text-transparent justify-between gap-6 md:gap-2 lg:justify-between bg-gradient-to-r from-sky-100 to-sky-200 bg-clip-text sm:text-sm lg:text-2xl lg:gap-16">
                             <li>
