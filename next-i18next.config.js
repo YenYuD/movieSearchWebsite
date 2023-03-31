fdbfd9b8136e606185f1782e174b9905cf080dfc
YenYuD <diaoemily0830@gmail.com>
2023-03-31 16:34:01 +0800

    debugging

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require("path");
 module.exports = {
     i18n: {
         locales: ["default", "en", "zh-TW"],
         defaultLocale: "default",
         localeDetection: true,
+        localePath: path.resolve("./public/locales/"),
+        localeStructure: "{{lng}}/{{ns}}",
     },
 };
