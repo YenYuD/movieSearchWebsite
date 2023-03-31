224cfee1152ab2562df3f7a889e80422fe88e89c
YenYuD <diaoemily0830@gmail.com>
2023-03-31 16:38:45 +0800

    i18n.config補上defaultLocale

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 module.exports = {
     i18n: {
         locales: ["en", "zh-TW"],
+        defaultLocale: "zh-TW",
         localeDetection: true,
         localePath: path.resolve("./public/locales/"),
         localeStructure: "{{lng}}/{{ns}}",
