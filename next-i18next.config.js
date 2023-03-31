2562c054d58b87a6b0b4c1eb5b708c4f5f0bfc85
YenYuD <diaoemily0830@gmail.com>
2023-03-31 16:37:33 +0800

    解決useTranslation 在vercel中不起作用的問題

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,7 @@
 const path = require("path");
 module.exports = {
     i18n: {
-        locales: ["default", "en", "zh-TW"],
-        defaultLocale: "default",
+        locales: ["en", "zh-TW"],
         localeDetection: true,
         localePath: path.resolve("./public/locales/"),
         localeStructure: "{{lng}}/{{ns}}",
